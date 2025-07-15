434864e65a870e7120cb63a0e3f9b68ba38333fa
Anna Parilova <187332467+ssendns@users.noreply.github.com>
2025-07-15 14:41:53 +0300

    todo class created

---
+++ src/modules/todo.js
@@ -0,0 +1,8 @@
+export default class Todo {
+    constructor(title, description) {
+        this.id = crypto.randomUUID();
+        this.title = title;
+        this.description = description;
+        this.status = false;
+    }
+}
