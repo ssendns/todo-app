b38b3f4025b1d21ebc73296e4355b392ecba0cb6
Anna Parilova <187332467+ssendns@users.noreply.github.com>
2025-07-15 15:37:47 +0300

    more functionality created

--- src/modules/manager.js
+++ src/modules/manager.js
@@ -3,6 +3,7 @@ import List from './list.js';
 export default class Manager {
     constructor() {
         this.lists = [];
+        this.currentList = null;
     }
 
     addList(title) {
@@ -17,5 +18,33 @@ export default class Manager {
 
     getLists() {
         return this.lists;
+    }
+
+    removeList(id) {
+        this.lists = this.lists.filter(list => list.id != id);
+    }
+
+    editList(id, newTitle) {
+        const list = this.getList(id);
+        list.title = newTitle;
+    }
+
+    getCurrentList() {
+        return this.getList(currentList);
+    }
+
+    getTodos(id) {
+        return this.getList(id).getTodos();
+    }
+
+    removeTodo(listId, todoId) {
+        const list = this.getList(listId);
+        list.removeTodo(todoId);
+    }
+
+    changeTodoStatus(listId, todoId) {
+        const list = this.getList(listId);
+        const todo = list.getTodo(todoId);
+        todo.changeStatus();
     }
 }
