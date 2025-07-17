2edb17002bb57f831f566b9e863b5fa6ab29cfc8
Anna Parilova <187332467+ssendns@users.noreply.github.com>
2025-07-17 22:14:25 +0300

    change prompt to form

--- src/modules/displayController.js
+++ src/modules/displayController.js
@@ -1,93 +1,108 @@
-import Manager from './manager.js';
+import Manager from "./manager.js";
 
 const displayController = (function () {
-    const manager = new Manager();
-    const listContainer = document.querySelector('#list-container');
-    const todoContainer = document.querySelector('#todo-container');
-    const currentListHeader = document.querySelector('#cur');
+  const manager = new Manager();
+  const listContainer = document.querySelector("#list-container");
+  const todoContainer = document.querySelector("#todo-container");
+  const currentListHeader = document.querySelector("#cur");
+  const listForm = document.querySelector("#list-form");
+  const todoForm = document.querySelector("#todo-form");
 
-    const addListBtn = document.querySelector('#add-list');
-    addListBtn.addEventListener("click", () => {
-        listContainer.innerHTML = "";
-        const title = prompt("title:");
-        manager.addList(title);
-        displayController.renderLists();
-    });
+  const addListBtn = document.querySelector("#add-list");
+  addListBtn.addEventListener("click", () => {
+    listForm.classList.toggle("hidden");
+  });
 
-    const addTodoBtn = document.querySelector('#add-todo');
-    addTodoBtn.addEventListener("click", () => {
-        todoContainer.innerHTML = "";
-        const title = prompt("title:");
-        const description = prompt("description:");
-        manager.addTodo(manager.currentList, title, description);
-        displayController.renderTodos();
-    });
+  listForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const name = document.querySelector("#list-name").value.trim();
+    const color = document.querySelector("#color").value.trim();
+    manager.addList(name, color);
+    e.target.reset();
+    e.target.classList.add("hidden");
+    displayController.renderLists();
+  });
 
-    function renderLists() {
-        listContainer.innerHTML = "";
-        const lists = manager.lists;
-        for(let i = 0; i < lists.length; i++) {
-            const list = lists[i];
-    
-            const card = document.createElement("div");
-            card.classList.add("list");
-            if (list.id === manager.currentList) {
-                card.id = "selected";
-            } else {
-                card.id = "not-selected";
-            }
+  const addTodoBtn = document.querySelector("#add-todo");
+  addTodoBtn.addEventListener("click", () => {
+    todoForm.classList.toggle("hidden");
+  });
 
-            card.innerHTML = `<p>${list.title}</p>`;
+  todoForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const title = document.querySelector("#todo-title").value.trim();
+    const description = document.querySelector("#todo-desc").value.trim();
+    manager.addTodo(manager.currentList, title, description);
+    e.target.reset();
+    e.target.classList.add("hidden");
+    displayController.renderTodos();
+  });
 
-            card.addEventListener("click", () => {
-                manager.currentList = list.id;
-                renderTodos();
-                renderLists();
-            });
+  function renderLists() {
+    listContainer.innerHTML = "";
+    const lists = manager.lists;
+    for (let i = 0; i < lists.length; i++) {
+      const list = lists[i];
 
-            listContainer.appendChild(card);
-        }
+      const card = document.createElement("div");
+      card.classList.add("list");
+      if (list.id === manager.currentList) {
+        card.id = "selected";
+      } else {
+        card.id = "not-selected";
+      }
+
+      card.innerHTML = `<p>${list.title}</p>`;
+
+      card.addEventListener("click", () => {
+        manager.currentList = list.id;
+        renderTodos();
+        renderLists();
+      });
+
+      listContainer.appendChild(card);
     }
+  }
 
-    function renderTodos() {
-        const list = manager.getCurrentList();
-        todoContainer.innerHTML = "";
-        currentListHeader.textContent = list.title;
-        const todos = list.todos;
-        for(let i = 0; i < todos.length; i++) {
-            const todo = todos[i];
-    
-            const card = document.createElement("div");
-            card.classList.add("todo");
-            if (todo.status) {
-                card.id = "done";
-            } else {
-                card.id = "not-done";
-            }
+  function renderTodos() {
+    const list = manager.getCurrentList();
+    todoContainer.innerHTML = "";
+    currentListHeader.textContent = list.title;
+    const todos = list.todos;
+    for (let i = 0; i < todos.length; i++) {
+      const todo = todos[i];
 
-            card.innerHTML = `
+      const card = document.createElement("div");
+      card.classList.add("todo");
+      if (todo.status) {
+        card.id = "done";
+      } else {
+        card.id = "not-done";
+      }
+
+      card.innerHTML = `
                 <input type="checkbox" ${todo.status ? "checked" : ""} />
                 <label>${todo.title}</label>
                 <button id="remove">remove todo</button>
             `;
 
-            const checkbox = card.querySelector("input[type='checkbox']");
-            checkbox.addEventListener("change", () => {
-                manager.changeTodoStatus(manager.currentList, todo.id);
-                renderTodos();
-            });
+      const checkbox = card.querySelector("input[type='checkbox']");
+      checkbox.addEventListener("change", () => {
+        manager.changeTodoStatus(manager.currentList, todo.id);
+        renderTodos();
+      });
 
-            const removeBtn = card.querySelector("#remove");
-            removeBtn.addEventListener("click", () => {
-                manager.removeTodo(manager.currentList, todo.id);
-                renderTodos();
-            });
+      const removeBtn = card.querySelector("#remove");
+      removeBtn.addEventListener("click", () => {
+        manager.removeTodo(manager.currentList, todo.id);
+        renderTodos();
+      });
 
-            todoContainer.appendChild(card);
-        }
+      todoContainer.appendChild(card);
     }
-  
-    return { renderLists, renderTodos, manager };
+  }
+
+  return { renderLists, renderTodos };
 })();
-  
+
 export default displayController;
